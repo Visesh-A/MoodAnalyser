ed7069e449b77c588f0af25523288ecd068623b8
“Visesh-A” <viseshajl@gmail.com>
2023-04-02 21:26:15 +0530

    [visesh]Add: Empty mood exception

--- MoodAnalyzerProblems/MoodAnalyzer.cs
+++ MoodAnalyzerProblems/MoodAnalyzer.cs
@@ -9,13 +9,17 @@
         }
         public string AnalyzeMood()
         {
+            if (this.message.Equals(string.Empty))
+            {
+                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.EMPTY_MOOD, "Message is Empty");
+            }
             try
             {
-                if (message.ToLower().Contains("sad"))
+                if (this.message.ToLower().Contains("sad"))
                     return "SAD";
                 return "HAPPY";
             }
-            catch (MoodAnalyzerException)
+            catch (Exception)
             {
                 throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NULL_MOOD, "Message is Null");
             }
