bf5c5fa806ae851ccada21588380e48004cc96d8
“Visesh-A” <viseshajl@gmail.com>
2023-04-02 21:17:11 +0530

    [visesh]Add: Exception

--- MoodAnalyzerProblems/MoodAnalyzer.cs
+++ MoodAnalyzerProblems/MoodAnalyzer.cs
@@ -9,9 +9,16 @@
         }
         public string AnalyzeMood()
         {
-            if (message.ToLower().Contains("sad"))
-                return "SAD";
-            return "HAPPY";
+            try
+            {
+                if (message.ToLower().Contains("sad"))
+                    return "SAD";
+                return "HAPPY";
+            }
+            catch (Exception ex)
+            {
+                return "HAPPY";
+            }
         }
     }
 }
