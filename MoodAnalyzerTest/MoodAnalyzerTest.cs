cfd276173c4c99281fa3f87d541a19dd1e3f99ad
“Visesh-A” <viseshajl@gmail.com>
2023-04-02 21:21:01 +0530

    [visesh]Add: Custom Exception

--- MoodAnalyzerTest/MoodAnalyzerTest.cs
+++ MoodAnalyzerTest/MoodAnalyzerTest.cs
@@ -15,10 +15,24 @@ namespace MoodAnalyzerTest
         [Test]
         public void GivenAnyMood_WhenAnalyze_ShouldReturnSad()
         {
-            string message = "I am in Any Mood";
+            string message = "I am in a Any Mood";
             MoodAnalyzer analyzer = new MoodAnalyzer(message);
             string result = analyzer.AnalyzeMood();
             Assert.AreEqual("HAPPY", result);
+        }
+        [Test]
+        public void GivenNullMood_ShouldThrow_Exception()
+        {
+            try
+            {
+                string message = "null";
+                MoodAnalyzer analyzer = new MoodAnalyzer(message);
+                string result = analyzer.AnalyzeMood();
+            }
+            catch (MoodAnalyzerException ex)
+            {
+                Assert.AreEqual(ex.Message, "Message is Null");
+            }
         }
     }
 }
